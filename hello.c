4b739971a2cf7fa8cc444faa408c62cecc545821
Doug Young <dougy@cortexnology.com>
2014-12-04 15:38:22 -0700

    Update hello.c
    Fixed it!

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(void)
   // 1. prove I can modify the code, and
   // 2. to provoke a bad build, which will then show up in Jeff's spiffy top-level README.md
   // -dougy 12/4/2014
-  printf("hello, world\n);
+  // Fixed it!
+  // -dougy 12/4/2014
+  printf("hello, world\n");
   return 0;
 }
