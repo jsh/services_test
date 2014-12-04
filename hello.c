6cff9f80f6560749e0d16a11c69f808f13e37be6
Doug Young <dougy@cortexnology.com>
2014-12-04 15:14:23 -0700

    Introduced a compilation error
    Introducing a compilation error in order to:
    1. prove I can modify the code, and
    2. to provoke a bad build, which will then show up in Jeff's spiffy top-level README.md

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-  printf("hello, world\n");
+  // Introducing a compilation error in order to:
+  // 1. prove I can modify the code, and
+  // 2. to provoke a bad build, which will then show up in Jeff's spiffy top-level README.md
+  // -dougy 12/4/2014
+  printf("hello, world\n);
   return 0;
 }
