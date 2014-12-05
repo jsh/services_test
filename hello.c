956c17a834cca90fe6e6610d15b7519e3c5273ad
Jeff Haemer <jhaemer@gogoair.com>
2014-12-05 11:28:30 -0700

    Remove logging comments.
    Logging comments go in the log, not in the code.

--- hello.c
+++ hello.c
@@ -2,12 +2,6 @@
 
 int main(void)
 {
-  // Introducing a compilation error in order to:
-  // 1. prove I can modify the code, and
-  // 2. to provoke a bad build, which will then show up in Jeff's spiffy top-level README.md
-  // -dougy 12/4/2014
-  // Fixed it!
-  // -dougy 12/4/2014
   printf("hello, world\n");
   return 0;
 }
